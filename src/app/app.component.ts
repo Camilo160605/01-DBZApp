db7e26289fbafb8f15c0810578e74ad6ae8336b5
Camilo160605 <camilo1665ober@gmail.com>
2023-10-13 21:48:20 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'my-personal-proyect';
+  title2 : string = 'Dbz Characters In Angular'
+  sidebarOpen : boolean = false;
+
+  ngOnInit(): void {}
+
+  toggleSidebar(){
+    this.sidebarOpen = !this.sidebarOpen;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
   ],
+
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DbzModule
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
